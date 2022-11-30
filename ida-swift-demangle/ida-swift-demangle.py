45fd7411b2e5b0574b4660c462c8f9d9ca10e475
Donato Fiore <50346119+donato-fiore@users.noreply.github.com>
2022-11-30 14:56:52 -0500

    Update ida-swift-demangle.py
    Update old code to work in 2022
    Remove unnecessary imports

--- ida-swift-demangle/ida-swift-demangle.py
+++ ida-swift-demangle/ida-swift-demangle.py
@@ -1,45 +1,41 @@
-# -*- coding: utf-8 -*-
+import os
+import platform
+import subprocess
 
 import idautils
 import idc
-from idaapi import PluginForm
-import operator
-import csv
-import sys
-import os
-import json
-import subprocess
-import platform
 
 
 def demangle_exe_path():
     sysstr = platform.system()
-    if(sysstr =="Darwin"):
-        return '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/swift-demangle'
-    if(sysstr =="Windows"):
-        directory=os.path.split(os.path.realpath(__file__))[0]
-        return directory + '/swift-demangle.exe'
+    if sysstr == "Darwin":
+        return "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/swift-demangle"
+    if sysstr == "Windows":
+        directory = os.path.split(os.path.realpath(__file__))[0]
+        return directory + "/swift-demangle.exe"
 
     raise "Only support macOS and Windows"
 
 
-
-def demangle(func_list):
-    proc = subprocess.Popen(demangle_exe_path(),stdout=subprocess.PIPE,stdin=subprocess.PIPE)
-    proc.stdin.write('\n'.join(func_list))
+def demangle(func_list: list):
+    proc = subprocess.Popen(
+        demangle_exe_path(), stdout=subprocess.PIPE, stdin=subprocess.PIPE
+    )
+    proc.stdin.write("\n".join(func_list).encode("utf-8"))
     proc.stdin.close()
-    out = proc.stdout.read()
+    out = proc.stdout.read().decode("utf-8")
     proc.wait()
-    result = out.split('\n')
+    result = out.split("\n")
     return result
+
 
 AllFunc = []
 AllFuncName = []
 for func in idautils.Functions():
-    name = GetFunctionName(func)
+    name = idc.get_func_name(func)
 
     AllFunc += [func]
-    if name [:1] == "_":
+    if name[:1] == "_":
         name = name[1:]
     AllFuncName += [name]
 
@@ -50,15 +46,14 @@ demangleList = demangle(AllFuncName)
 # demangle swift function name must be wrap in "-[demangle name]" or "+[demangle name]"
 def wrapSwiftInOCMethod(funcName):
     funcName = funcName.strip()
-    if funcName.startswith('-[') or funcName.startswith('+['):
+    if funcName.startswith("-[") or funcName.startswith("+["):
         return funcName
-    if funcName.startswith('static '):
-        return  "+[ " + funcName + ' ]'
-    return  "-[ " + funcName + ' ]'
+    if funcName.startswith("static "):
+        return "+[ " + funcName + " ]"
+    return "-[ " + funcName + " ]"
 
 
-
-for i in xrange(len(AllFunc)):
+for i in range(len(AllFunc)):
     addr = AllFunc[i]
     old_name = AllFuncName[i]
     full_name = demangleList[i]
@@ -67,9 +62,6 @@ for i in xrange(len(AllFunc)):
         new_name = full_name.split("->")[0]
     # new_name = new_name.replace("@objc ", "_")
     if new_name != old_name:
-        new_name = wrapSwiftInOCMethod(new_name);
-        MakeNameEx(addr, new_name, SN_NOCHECK|SN_NOWARN)
-        SetFunctionCmt(addr, "%s \n%s" % (old_name, full_name), 1)
-
-
-
+        new_name = wrapSwiftInOCMethod(new_name)
+        idc.set_name(addr, new_name, SN_NOCHECK | SN_NOWARN)
+        idc.set_func_cmt(addr, "%s \n%s" % (old_name, full_name), 1)
